a491c97e20d72d115177bc784b295610c621d16f
JUAN DAVID GIL TAMAYO <juangil211237@correo.itm.edu.co>
2020-04-23 12:47:06 -0500

    Add API

---
+++ Expenditure.Web/Controllers/API/ExpendituresController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Expenditure.Web.Data.Entities;
+
+namespace Expenditure.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpendituresController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ExpendituresController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Expenditures
+        [HttpGet]
+        public IEnumerable<ExpenditureEntity> GetExpenses()
+        {
+            return _context.Expenses;
+        }
+
+        // GET: api/Expenditures/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetExpenditureEntity([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var expenditureEntity = await _context.Expenses.FindAsync(id);
+
+            if (expenditureEntity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(expenditureEntity);
+        }
+
+    }
+}
---
+++ Expenditure.Web/Controllers/API/TravelsController.cs
@@ -0,0 +1,31 @@
+﻿using Expenditure.Web.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenditure.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TravelsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TravelsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTravels()
+        {
+            List<TravelEntity> Travels = await _context.Travels
+                .Include(t => t.Expenses)
+                .ToListAsync();
+            return Ok(Travels);
+        }
+    }
+}
